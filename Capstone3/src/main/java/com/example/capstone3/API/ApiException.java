de3f5796977303b5e9aa54467802dc9d1b35a18b
nahjalabbad <146772725+nahjalabbad@users.noreply.github.com>
2024-04-18 16:59:49 +0300

    Delete Capstone3 directory

--- Capstone3/src/main/java/com/example/capstone3/API/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.capstone3.API;
-
-public class ApiException extends RuntimeException {
-    public ApiException(String message){
-        super(message);
-    }
-
-}
--- Capstone3/src/main/java/com/example/capstone3/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.capstone3.Repository;
-
-import com.example.capstone3.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-User findUserByUserId (Integer userId);
-}
